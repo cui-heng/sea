d6ba386475f8a7c6b549a85b4f85557a7c0cb065
js-he <cuijiuhe@163.com>
2024-07-06 21:56:54 +0800

    fix: 修改nuxt

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 module.exports = {
-  dev: {
+  development: {
       NODE_ENV: 'dev',
       URL_PREFIX: '/website/common',
-      BASE_URL: 'http://localhost:8302',
+      BASE_URL: 'http://47.115.209.54:8080',
       // BASE_URL: 'http://***',
   },
-  prod: {
+  production: {
       NODE_ENV: 'prod',
       URL_PREFIX: '/api/front',
       BASE_URL: 'http://47.115.209.54:8080',
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,28 @@
 const env = require('./env');
 
 module.exports = {
+    alias: {
+        "@": "/",
+    },
     env: {
         baseUrl: env[process.env.NODE_ENV].BASE_URL,
         apiPrefix: env[process.env.NODE_ENV].URL_PREFIX
     },
 
     css: [
-        '@/assets/css/main.scss',
+        // '@/assets/css/main.scss',
     ],
-    plugins: [],
-    modules: [
-        '@nuxtjs/axios',
-        '@nuxtjs/proxy',
-        '@nuxtjs/style-resources'
+    plugins: [
+        {
+            src: '~plugins/extend',
+            ssr: true
+        }
     ],
+    // modules: [
+    //     '@nuxtjs/axios',
+    //     '@nuxtjs/proxy',
+    //     '@nuxtjs/style-resources'
+    // ],
     axios: {
         proxy: true,
         credentials: true,
@@ -40,13 +48,26 @@ module.exports = {
         script: [{
             // src: 'http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/??flexible_css.js,flexible.js'
         }],
-        meta: [{
+        meta: [
+            {
                 charset: 'utf-8'
             },
             {
                 name: 'viewport',
                 content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
             },
+            {
+                name: "keywords",
+                content:
+                    "期货,期货开户,期货手续费,期货公司,期货账户,期货保证金",
+                hid: "keywords",
+            },
+            {
+                name: "description",
+                content:
+                    "描述",
+                hid: "description",
+            }
         ]
     },
 
